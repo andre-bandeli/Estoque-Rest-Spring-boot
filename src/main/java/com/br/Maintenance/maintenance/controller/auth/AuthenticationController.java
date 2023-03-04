d4f87ec6984527278484782bfd82ab05548a09b4
andre-bandeli <andre.lbandeli@gmail.com>
2023-03-04 04:22:42 -0300

    .

--- src/main/java/com/br/Maintenance/maintenance/controller/auth/AuthenticationController.java
+++ src/main/java/com/br/Maintenance/maintenance/controller/auth/AuthenticationController.java
@@ -1,11 +1,13 @@
 package com.br.Maintenance.maintenance.controller.auth;
 
 import java.security.NoSuchAlgorithmException;
+import java.security.Principal;
 import java.security.spec.InvalidKeySpecException;
 
 import com.br.Maintenance.maintenance.configuration.jwt.JwtToken;
 import com.br.Maintenance.maintenance.model.auth.AuthenticationRequest;
 import com.br.Maintenance.maintenance.model.auth.AuthenticationResponse;
+import com.br.Maintenance.maintenance.model.user.UserDescriptionResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,11 +16,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -53,19 +51,15 @@ public class AuthenticationController {
         return ResponseEntity.ok(response);
     }
 
-//    @GetMapping("/auth/userinfo")
+//    @GetMapping("/auth/description")
+//    @CrossOrigin(origins = "http://localhost:3000/usuario")
 //    public ResponseEntity<?> getUserInfo(Principal user){
-//        User userObj=(User) userDetailsService.loadUserByUsername(user.getName());
+//        User userObj= (User) userDetailsService.loadUserByUsername(user.getName());
+//        UserDescriptionResponse descriptionResponse=new UserDescriptionResponse();
+//        descriptionResponse.setFirstName(userObj.getFirstName());
+//        descriptionResponse.setLastName(userObj.getLastName());
+//        descriptionResponse.setRoles(userObj.getAuthorities().toArray());
 //
-//        UserInfo userInfo=new UserInfo();
-//        userInfo.setFirstName(userObj.getFirstName());
-//        userInfo.setLastName(userObj.getLastName());
-//        userInfo.setRoles(userObj.getAuthorities().toArray());
-//
-//
-//        return ResponseEntity.ok(userInfo);
-//
-//
-//
+//        return ResponseEntity.ok(descriptionResponse);
 //    }
 }
